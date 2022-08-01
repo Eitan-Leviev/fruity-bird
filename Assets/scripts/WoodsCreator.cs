033423be75900e648a449224b273eda89a2eb6ca
eitan lev <eitanleviev111@gmail.com>
2022-08-01 14:30:04 +0300

    easier game
    acceleratingTime to private - just for now
    wood creation without sustain

--- Assets/scripts/WoodsCreator.cs
+++ Assets/scripts/WoodsCreator.cs
@@ -7,14 +7,14 @@ using SysRandom = System.Random;
 
 public class WoodsCreator : MonoBehaviour
 {
-    public int acceleratingTime; // every acceleratingTime seconds, woods will bo moving faster
+    private int acceleratingTime; // every acceleratingTime seconds, woods will bo moving faster
     
     private float maxTime;
     public float averageTime;
     public float deltaTime;
     private float timer = 0;
-    public float hight;
-    public float width;
+    public float height; // height range of woods (with middle of 0 - the WoodCreator's transform)
+    public float width; // height range of woods (with middle of 1 - the initial scale)
 
     public GameObject woodPrefab;
     
@@ -45,7 +45,8 @@ public class WoodsCreator : MonoBehaviour
 
     private void Start()
     {
-        maxTime = Random.Range(averageTime - deltaTime, averageTime + deltaTime);
+        // maxTime = Random.Range(averageTime - deltaTime, averageTime + deltaTime);
+        maxTime = -1;
         
         fruitFlag = rnd.Next(1, 5) > 1; // Probabilistic bias to appearance of fruits: 4/5
 
@@ -62,7 +63,7 @@ public class WoodsCreator : MonoBehaviour
             // create new wood at the right corner of the screen
             GameObject newWood = Instantiate(woodPrefab);
             // randomize the wood's height and width
-            newWood.transform.position = transform.position + new Vector3(0, Random.Range(-hight, hight), 0);
+            newWood.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
             newWood.transform.localScale = new Vector3(Random.Range(1 - width, 1 + width), 1, 1);
             // destroy wood after 15 sec'
             Destroy(newWood, 15);
@@ -83,7 +84,7 @@ public class WoodsCreator : MonoBehaviour
             // create new fruit at the right corner of the screen
             GameObject newFruit = Instantiate(fruit);
             // randomize height and fruit kind
-            newFruit.transform.position = transform.position + new Vector3(0, Random.Range(-hight * 2, hight * 2), 0);
+            newFruit.transform.position = transform.position + new Vector3(0, Random.Range(-height * 2, height * 2), 0);
             newFruit.GetComponent<SpriteRenderer>().sprite = fruitList[rnd.Next(0, 4)];
             // destroy wood fruit 15 sec'
             Destroy(newFruit, 15);
